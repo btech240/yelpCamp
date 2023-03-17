1614f5a0ad19ba57deb5a9b6161678ff6d92d9cc
btech240 <brenttlong@gmail.com>
2023-03-17 10:04:38 -0400

    Updated app.js and added files for basic read functionality.

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
+// Run connection to mongodb
+main().catch((err) => {
+    console.log("OH NO ERROR!!");
+    console.log(err);
+});
+// function to await calls to mongodb
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+    console.log("Mongo CONNECTION OPEN!!");
+}
+
+// Set express as app framework
+const app = express();
+
+// Set middleware, EJS to handle html
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground })
+})
+
+// This call seeds our database with data in the seed folder
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' });
+//     await camp.save();
+//     res.send(camp);
+// })
 
 app.listen(3000, () => {
     console.log('SERVER LISTENING...');
