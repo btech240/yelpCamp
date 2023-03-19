74bd26b2f84797a253c9153024b314ee20fbf5b1
btech240 <brenttlong@gmail.com>
2023-03-19 18:02:52 -0400

    Updated app.js and added files for new/edit/del functions

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+// Required modules
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 // Run connection to mongodb
 main().catch((err) => {
     console.log("OH NO ERROR!!");
     console.log(err);
 });
-// function to await calls to mongodb
+// Function to await calls to mongodb
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
     console.log("Mongo CONNECTION OPEN!!");
@@ -20,19 +22,55 @@ const app = express();
 // Set middleware, EJS to handle html
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
+// Home page
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
+// Show all campgrounds
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
+});
+
+// New campground form
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+// Provide an edit form, loading in the campground with the given ID
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
+
+// Edit the campground with the given ID
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
+// Process new campground form, saving it to the database
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// Finds campground by ID and shows details
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground })
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 // This call seeds our database with data in the seed folder
@@ -42,6 +80,7 @@ app.get('/campgrounds/:id', async (req, res) => {
 //     res.send(camp);
 // })
 
+// Set node to listen on port 3000
 app.listen(3000, () => {
     console.log('SERVER LISTENING...');
 })
