ce6336573c7806c5ef64e56da58cc1c34b701b60
btech240 <brenttlong@gmail.com>
2023-04-29 13:46:46 -0400

    Added express-sessions to app and set up basic session config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const  { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
@@ -35,6 +36,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 // Serve the static directories - using join to serve all directories within public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Create session and cookie
+const sessionConfig = {
+    secret: 'thisshouldbebetter',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 // Use express router middleware
 app.use('/campgrounds', campgrounds);
