581c1e128fd57086d0da20f5f7c8ef8a36e3df69
btech240 <brenttlong@gmail.com>
2023-05-02 17:28:28 -0400

    Added routes/users and router middleware to app.js

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 // Run connection to mongodb
 main().catch((err) => {
@@ -77,8 +78,9 @@ app.get('/fakeuser', async (req,res) => {
 })
 
 // Use express router middleware
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 // Home Page - Needs Fixed
 app.get('/', (req, res) => {
