0a00fa0eeea5ae551fe51d840513b4a14295d0b5
btech240 <brenttlong@gmail.com>
2023-05-02 20:28:15 -0400

    Added route to users.js to register a user

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 
+// Route to register page
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
-
-router.post('/register', (req, res) => {
-    res.send(req.body);
 });
+
+// Route to process user registration
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const {email, username, password} = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp');
+        res.redirect('/campgrounds');
+    } catch (err) {
+        req.flash('error', err.message)
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
