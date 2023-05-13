8e1aa1db369dc99d750eeec3bf56ad4fac3c06e5
btech240 <brenttlong@gmail.com>
2023-05-12 20:36:48 -0400

    Edited campgrounds.js slim routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
-const  { campgroundSchema, reviewSchema } = require('../schemas.js');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware.js');
 const campgrounds = require('../controllers/campgrounds');
-const ExpressError = require('../utilities/ExpressError');
-const Campground = require('../models/campground');
-const { populate } = require('../models/review');
 
-// Show all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    // Show all campgrounds
+    .get(catchAsync(campgrounds.index))
+    // Process new campground form, saving it to the database, protect post route with isLoggedIn middleware
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 // New campground form, protect login form with isLoggedIn middleware
+// New route needs to be placed before the /:id routes or it will think it is /:id/new and create an error
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.route('/:id')
+    // Edit the campground with the given ID, edit route with isLoggedIn middleware
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))
+    // Finds campground by ID and shows details
+    .get(catchAsync(campgrounds.show))
+    // Deletes campground with the given ID, protect delete route with isLoggedIn middleware
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 // Provide an edit form, loading in the campground with the given ID
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-// Edit the campground with the given ID, edit route with isLoggedIn middleware
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground));
-
-// Process new campground form, saving it to the database, protect post route with isLoggedIn middleware
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-// Finds campground by ID and shows details
-router.get('/:id', catchAsync(campgrounds.show));
-
-// Deletes campground with the given ID, protect delete route with isLoggedIn middleware
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 // Export this router for use in app.js
 module.exports = router;
