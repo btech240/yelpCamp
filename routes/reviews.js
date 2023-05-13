f7dda12fc1a4439004b572c0fa71fbec7cea1b2f
btech240 <brenttlong@gmail.com>
2023-05-12 20:47:34 -0400

    Edited users.js to slim routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const { validateReview, isLoggedIn, isReviewAuthor} = require('../middleware'); 
 // Require Express router, with params passed
-const router = express.Router({ mergeParams: true } ) ;
-const { campgroundSchema, reviewSchema } = require('../schemas.js');
-
+const router = express.Router({ mergeParams: true } );
 // Require the reviews controller
 const reviews = require('../controllers/reviews')
-
 // Require our error handling middleware
-const ExpressError = require('../utilities/ExpressError');
 const catchAsync = require('../utilities/catchAsync');
-
-// Require campground and review models
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 
 // Route to delete a campground review by pulling a review ID out of the campground
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
-const User = require('../models/user');
 const { storeReturnTo } = require('../middleware');
 
 // Include the user controller
 const users = require('../controllers/users');
 
-// Route to register page
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    // Route to register page
+    .get(users.renderRegisterForm)
+    // Route to process user registration
+    .post(catchAsync(users.createUser))
 
-// Route to process user registration
-router.post('/register', catchAsync(users.createUser));
-
-// Route to login page
-router.get('/login', users.renderLoginForm);
-
-// Route to process user log in
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    // Route to login page
+    .get(users.renderLoginForm)
+    // Route to process user log in
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 // Route to log out a user
-router.get('/logout', users.logout);
+router.get('/logout',users.logout)
 
 module.exports = router;
