06c48a638c28cf4c105d0533158c5b5b870daa34
btech240 <brenttlong@gmail.com>
2023-05-16 16:22:05 -0400

    Added delete image functionality to edit page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -35,6 +35,9 @@ module.exports.editCampground = async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(file => ({url: file.path, filename: file.filename}));
     campground.images.push(...imgs);
+    if (req.body.deleteImages) {
+        await campground.updateOne({ $pull: {images: {filename: {$in: req.body.deleteImages } } } });
+    }
     await campground.save();
     req.flash('success', 'Successfully updated campground.');
     res.redirect(`/campgrounds/${campground._id}`);
