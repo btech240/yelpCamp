e607b44927dd309e2cf93f3db7b08ff3d83d7e52
btech240 <brenttlong@gmail.com>
2023-05-17 19:00:47 -0400

    Added mapbox module, added geometry to campground model and logic to store it in controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require('../models/campground');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocoding ({ accessToken: mapBoxToken });
 const cloudinary = require('cloudinary');
 
 module.exports.index = async (req, res) => {
@@ -11,11 +14,19 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    // Find coordinates for input location    
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground);
+    // Save location data 
+    campground.geometry = geoData.body.features[0].geometry;
     // Loop over each image uploaded if more than one, and map their path and filename to campground schema
     campground.images = req.files.map(file => ({url: file.path, filename: file.filename}))
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'Successfully created a new campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }
