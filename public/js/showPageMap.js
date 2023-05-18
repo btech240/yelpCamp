fcc977ffc32c2b7dbd7452bca085eb478061a8f1
btech240 <brenttlong@gmail.com>
2023-05-18 09:17:16 -0400

    Brought campground coordinates into map. Now centers at input location. Bug: mapbox marker not showing

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,6 +3,10 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat([campground.geometry.coordinates])
+    .addTo(map)
