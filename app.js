db48f80981fe5766946ca27b6729c511c7f22cbe
btech240 <brenttlong@gmail.com>
2023-05-22 09:05:45 -0400

    Added express-mongo-sanitize and included/used it in app

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -41,6 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 // Serve the static directories - using join to serve all directories within public
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 // Create session and cookie
 const sessionConfig = {
